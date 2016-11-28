ee1f37f6a922f13a6255767a25115245397bfe95
zjien <694396727@qq.com>
2016-11-28 23:57:23 +0800

    added verifycode model

---
+++ src/Models/VerifyCode.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Zjien\Proton\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class VerifyCode extends Model
+{
+    protected $table = 'verify_codes';
+
+    protected $timestamp = false;
+}
