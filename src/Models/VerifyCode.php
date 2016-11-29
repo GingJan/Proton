c4d63fa169606b5240e1b3c3a4b8864966e3c624
zjien <694396727@qq.com>
2016-11-29 23:56:23 +0800

    added VerifyCode model

--- src/Models/VerifyCode.php
+++ src/Models/VerifyCode.php
@@ -9,4 +9,11 @@ class VerifyCode extends Model
     protected $table = 'verify_codes';
 
     protected $timestamp = false;
+
+    const TYPE_PC = 10;
+    const TYPE_PHONE = 20;
+    const TYPE_EMAIL = 30;
+
+    const AVAILABLE_TRUE = 1;
+    const AVAILABLE_FALSE = 0;
 }
